da8534a98637efc83cb41fea14b21b19e39efb92
ah9mon <eomgyeol97@gmail.com>
2023-07-14 09:01:27 +0900

    230713

--- Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,29 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    // 직접 스프링 빈에 등록하기
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
+
+/*    @Bean
+    public MemberRepository memberRepository() {
+
+        return new JpaMemberRepository(em);
+    }*/
 }
--- Spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,25 +1,42 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
 
     private final MemberService memberService;
-//    @Autowired private MemberService memberService;
-    /*
-    * 필드 주입 비추
-    * 변경 힘듬
-    *
-    * setter로 DI 주입하는 방법도 있음
-    * setter로 주입하면 public으로 노출됨 -> 조심해야함 -> 권장x
-    *
-    * 상황에 따라 구현 클래스 변경하려면 설정을 통해 등록하는게 좋음 -> 코드 수정 필요하지 않으므로
-    * */
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public  String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
